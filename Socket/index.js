7e034b539742951e2a7271ed081f69c766d9c42c
singhdeveloper1 <priyanshusingh874@gmail.com>
2025-04-14 18:47:28 +0530

    added in README file

--- Socket/index.js
+++ Socket/index.js
@@ -47,27 +47,12 @@ io.on("connection", async(socket)=>{
         io.emit("chat message" , data)
 
     })
-    
-    //! editing mesage
-    
-    // socket.on("edit message", async (message)=>{
-        //     console.log("edit data", message)
-        
-        //     const editMessage = await Message.findByIdAndUpdate(message._id,{
-            //         message : message.message
-            //     },{new : true})       
-            
-            // })
-            
+                
             //! sending updated message
         socket.on("update message",async (data)=>{
-            console.log("update", data.id)
-
             const updatedData = await Message.findByIdAndUpdate(data.id,{
                 message : data.message
             },{new : true})
-
-            console.log(updatedData)
 
             io.emit("update message", updatedData)
         })
