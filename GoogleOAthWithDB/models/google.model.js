39f8eb2707b739e60d416dfc8822cf6017dfe028
singhdeveloper1 <priyanshusingh874@gmail.com>
2025-04-15 15:25:20 +0530

    created model to store user data when user auhenticate through google after that implemented google OAth code in new file i.e passport.jd

---
+++ GoogleOAthWithDB/models/google.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const googleSchema = new mongoose.Schema({
+    googleId : {
+        type : String,
+        required : true,
+        unique : true
+    },
+
+    name : {
+        type : String,
+        required : true,
+    },
+
+    email : {
+        type : email,
+        required : true,
+        unique : true
+    },
+
+    expiry : {
+        type : Date,
+        default : Date.now,
+        expires : 7200
+    }
+},{timestamps : true})
+
+const Google = mongoose.model("Google", googleSchema)
+
+export default Google
